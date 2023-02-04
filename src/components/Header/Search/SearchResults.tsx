7e51eb4a26ae7ab0b6528f51524edf9e97687b36
fleshr <sfrxfleshr@gmail.com>
2023-02-04 14:39:52 +0500

    SearhcResults overlay fix

--- src/components/Header/Search/SearchResults.tsx
+++ src/components/Header/Search/SearchResults.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const SearchResults: React.FC<Props> = ({ results, onLocaionClick }) => {
   return (
-    <div className="absolute top-0 left-0 w-full bg-dark-blue-600 px-[1.1875rem] pt-12 md:rounded-lg md:bg-dark-blue-500 md:px-2.5 md:pt-9">
+    <div className="absolute top-0 left-0 z-10 w-full bg-dark-blue-600 px-[1.1875rem] pt-12 md:rounded-lg md:bg-dark-blue-500 md:px-2.5 md:pt-9">
       <ul className="space-y-2 pt-1 pb-3 md:space-y-1.5 md:border-t md:border-dark-blue-400 md:py-2">
         {results.map((location) => (
           <li key={`${location.lat}${location.lon}`}>
