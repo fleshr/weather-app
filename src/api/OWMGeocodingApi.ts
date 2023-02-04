b1da6a42591c29f2a9e3ff588d69bf342f2097ea
fleshr <sfrxfleshr@gmail.com>
2023-02-04 14:34:53 +0500

    location fix

--- src/api/OWMGeocodingApi.ts
+++ src/api/OWMGeocodingApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const OWMGeocodingApi = axios.create({
-  baseURL: 'http://api.openweathermap.org/geo/1.0',
+  baseURL: 'https://api.openweathermap.org/geo/1.0',
   params: {
     appid: process.env.NEXT_PUBLIC_OWM_API_KEY,
     limit: 5,
