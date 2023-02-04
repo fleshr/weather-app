2e79b1ca736f40ec3e6b55d07f78bc79971ef3c3
fleshr <sfrxfleshr@gmail.com>
2023-02-04 17:51:42 +0500

    added loading skeleton for forecast component

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -5,9 +5,9 @@ import { IOWMForecast } from '@/interfaces/OWMForecast';
 import { minimizeForecast } from '@/utils';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import Loader from '../Loader';
 import ForecastDayRow from './ForecastDayRow';
 import ForecastDaytimeRow from './ForecastDaytimeRow';
+import ForecastSkeleton from './ForecastSkeleton';
 import ForecastTableHeader from './ForecastTableHeader';
 
 const Forecast = () => {
@@ -31,30 +31,27 @@ const Forecast = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
-  if (loading)
-    return (
-      <section className="flex h-60 items-center justify-center bg-dark-blue-600 md:h-80 md:rounded-[0.625rem]">
-        <Loader />
-      </section>
-    );
-
   return (
     <section className="bg-dark-blue-600 px-3.5 pt-2 md:rounded-[0.625rem] md:px-[1.875rem] md:pt-4">
       <h2 className="font-medium md:text-xl">Прогноз погоды</h2>
       <div className="mt-1.5 md:-mt-[1.1875rem] xl:-mt-[1.6875rem]">
         <ForecastTableHeader />
         <div className="divide-y divide-dark-blue-500">
-          {forecast?.map((item) => (
-            <ForecastDayRow time={item.time} key={item.time}>
-              {item.forecast.map((item) => (
-                <ForecastDaytimeRow
-                  key={item.daytime}
-                  weather={item.weather}
-                  time={item.daytime}
-                />
-              ))}
-            </ForecastDayRow>
-          ))}
+          {loading ? (
+            <ForecastSkeleton />
+          ) : (
+            forecast?.map((item) => (
+              <ForecastDayRow time={item.time} key={item.time}>
+                {item.forecast.map((item) => (
+                  <ForecastDaytimeRow
+                    key={item.daytime}
+                    weather={item.weather}
+                    time={item.daytime}
+                  />
+                ))}
+              </ForecastDayRow>
+            ))
+          )}
         </div>
       </div>
     </section>
---
+++ src/components/Forecast/ForecastSkeleton.tsx
@@ -0,0 +1,45 @@
+const ForecastSkeleton = () => {
+  return (
+    <>
+      {Array(5)
+        .fill(null)
+        .map((_, index) => {
+          return (
+            <div key={index} className="py-4 md:flex md:py-7">
+              <div className="md:flex md:w-[8.75rem] md:flex-shrink-0 md:items-center">
+                <div className="flex md:flex-col">
+                  <div className="h-[14px] w-[70px] animate-pulse rounded bg-dark-blue-400 md:h-[16px]" />
+                  <div className="ml-1 h-[14px] w-[50px] animate-pulse rounded bg-dark-blue-400 md:ml-0 md:mt-2" />
+                </div>
+              </div>
+              <div className="mt-1.5 space-y-1.5 md:mt-0 md:flex md:flex-grow md:flex-col md:justify-center md:space-y-3.5">
+                {Array(4)
+                  .fill(null)
+                  .map((_, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="grid grid-cols-forecast-mobile items-center md:grid-cols-forecast-4 md:gap-5 lg:grid-cols-forecast-5 xl:grid-cols-forecast-7"
+                      >
+                        <div className="h-[16px] w-[45px] animate-pulse rounded bg-dark-blue-400 xl:w-[50px]" />
+                        <div className="order-3 ml-2 h-[16px] w-[30px] animate-pulse rounded bg-dark-blue-400 md:order-none md:mx-auto" />
+                        <div className="order-2 md:order-none md:flex md:items-center">
+                          <div className="m-0.5 h-[22px] w-[22px] animate-pulse rounded-full bg-dark-blue-400" />
+                          <div className="hidden h-[16px] w-[190px] animate-pulse rounded bg-dark-blue-400 md:ml-3 md:block" />
+                        </div>
+                        <div className="order-4 ml-auto h-[16px] w-[70px] animate-pulse rounded bg-dark-blue-400 md:order-none md:mx-auto md:w-[50px]" />
+                        <div className="mx-auto hidden h-[16px] w-[40px] animate-pulse rounded bg-dark-blue-400 xl:block" />
+                        <div className="mx-auto hidden h-[16px] w-[40px] animate-pulse rounded bg-dark-blue-400 xl:block" />
+                        <div className="mx-auto hidden h-[16px] w-[30px] animate-pulse rounded bg-dark-blue-400 lg:block" />
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+export default ForecastSkeleton;
